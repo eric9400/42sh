aef460f1f21ff7a1e96d7bcac225ed7f28f5a7ff
Ernest Bardon <ernest.bardon@epita.fr>
2023-01-07 00:17:10 +0100

    fix: lexer step 1 is now fully functionnal

--- src/include/token.h
+++ src/include/token.h
@@ -1,20 +1,21 @@
 #ifndef TOKEN_H
 #define TOKEN_H
 
-enum Token_type
+
+enum tok_type
 {
     //conditional tokens
     IF, ELSE, THEN, ELIF, FI,
 
     //syntax 
-    SEMICOLON, NEWLINE, S_QUOTE, 
-    
-    WORDS
+    SEMICOLON, NEWLINE, S_QUOTE,
+
+    WORD
 };
 
-struct Token
+struct token
 {
-    enum Token_type t_type;
+    enum tok_type type;
     char *data;
 };
 
