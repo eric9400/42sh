f4b25e8e5f47e0b450103969a2a4a13a90e1e57d
Eric Baysengchanh <eric.baysengchanh@epita.fr>
2023-01-07 11:48:20 +0100

    feat: struct vector done

--- src/include/vector.h
+++ src/include/vector.h
@@ -3,12 +3,20 @@
 
 #include <stddef.h>
 
+struct vector
+{
+    size_t size;
+    size_t capacity;
+    char **data;        
+};
+
 struct vector *vector_init(size_t n);
+
 void vector_destroy(struct vector *v);
 struct vector *vector_remove(struct vector *v, size_t i);
 struct vector *vector_resize(struct vector *v, size_t n);
-struct vector *vector_append(struct vector *v, int elt);
+struct vector *vector_append(struct vector *v, char *s);
 void vector_print(const struct vector *v);
 struct vector *vector_reset(struct vector *v, size_t n);
 
-#endif /*VECTOR_H*/
+#endif /* VECTOR_H */
