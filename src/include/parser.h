ab70f25cb26267f04136240e228f8d55382b8725
Ernest Bardon <ernest.bardon@epita.fr>
2023-01-07 10:55:14 +0100

    feat: parser.c created

--- src/include/parser.h
+++ src/include/parser.h
@@ -1,4 +1,9 @@
 #ifndef PARSER_H
 #define PARSER_H
 
+#include "lexer.h"
+#include "ast.h"
+
+struct ast input(void);
+
 #endif /* PARSER_H */
---
+++ src/parser/parser.c
@@ -0,0 +1,58 @@
+#include "parser.h"
+
+static lexer *lex = init_lexer();
+
+struct ast input(void)
+{
+    //TODO
+}
+
+static struct ast list(void)
+{
+    //TODO
+}
+
+static struct ast and_or(void)
+{
+    //TODO
+}
+
+static struct ast pipeline(void)
+{
+    //TODO
+}
+
+static struct ast command(void)
+{
+    //TODO
+}
+
+static struct ast simple_command(void)
+{
+    //TODO
+}
+
+static struct ast element(void)
+{
+    //TODO
+}
+
+static struct ast rule_if(void)
+{
+i   //TODO
+}
+
+static struct ast shell_command(void)
+{
+    //TODO
+}
+
+static struct ast else_clause(void)
+{
+    //TODO
+}
+
+static struct ast compound_list(void)
+{
+    //TODO
+}
