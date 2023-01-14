a02dc49d805dbee3c77f56d221e072779b8fc147
Eric Bay <eric.baysengchanh@epita.fr>
2023-01-14 18:04:46 +0100

    fix: echo weird case

--- src/builtin/builtin.c
+++ src/builtin/builtin.c
@@ -3,12 +3,14 @@
 
 #include "vector.h"
 
-size_t print_special_char(int f_e, char c)
+static size_t print_special_char(int f_e, char c, int in_d_quotes)
 {
     if (!f_e)
     {
-        printf("\\");
-        return 0;
+        if (in_d_quotes)
+            printf("\\");
+        printf("%c", c);
+        return 1;
     }
 
     size_t res = 1;
@@ -27,20 +29,35 @@ size_t print_special_char(int f_e, char c)
     return res;
 }
 
-void print_echo(int f_n, int f_e, struct vector *v)
+static void print_echo(int f_n, int f_e, struct vector *v)
 {
+    int in_s_quotes= 0;
+    int in_d_quotes= 0;
     for (size_t i = 0; i < v->size; i++)
     {
         char *s = v->data[i];
         for (size_t j = 0; j < strlen(s); j++)
         {
-            if (s[j] == '\\')
-                j += print_special_char(f_e, s[j + 1]);
-            else if (s[j] == '\'' || s[j] == '"')
+            if (!in_d_quotes && s[j] == '\'')
+            {
+                in_s_quotes = !in_s_quotes;
                 continue;
+            }
+            else if (!in_s_quotes && s[j] == '"')
+            {
+                in_d_quotes = !in_d_quotes;
+                continue;
+            }
+            else if (in_s_quotes)
+                printf("%c", s[j]);
+            // we have to expand everything from here
+            else if (s[j] == '\\')
+                j += print_special_char(f_e, s[j + 1], in_d_quotes);
             else
                 printf("%c", s[j]);
         }
+        in_s_quotes = 0;
+        in_d_quotes = 0;
         if (i != v->size - 1)
             printf(" ");
     }
@@ -48,7 +65,7 @@ void print_echo(int f_n, int f_e, struct vector *v)
         printf("\n");
 }
 
-int is_flag(char *s, int *f_n, int *f_e)
+static int is_flag(char *s, int *f_n, int *f_e)
 {
     if (!s)
         return 1;
@@ -58,7 +75,8 @@ int is_flag(char *s, int *f_n, int *f_e)
     // can be a flag
     if (s[0] == '-')
     {
-        for (size_t i = 1; i < strlen(s); i++)
+        size_t len = strlen(s);
+        for (size_t i = 1; i < len; i++)
         {
             if (s[i] == 'n')
                 *f_n = 1;
